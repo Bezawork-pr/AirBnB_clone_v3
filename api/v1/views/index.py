8979c7321e0cffa1d36793b5168e6b6b2dd66446
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-15 00:29:16 -0800

    Count and display how many users, cities ... in storage in json format

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 Index file
 """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, Flask
+from models import storage
 
 
+app = Flask(__name__)
 @app_views.route('/status', strict_slashes=False)
 def status():
+    """Return status ok"""
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """Count using count method in storage"""
+    obj = {}
+    obj['amenities'] = storage.count('Amenity')
+    obj['cities'] = storage.count('City')
+    obj['places'] = storage.count('Place')
+    obj['reviews'] = storage.count('Review')
+    obj['states'] = storage.count('State')
+    obj['users'] = storage.count('User')
+    return jsonify(obj)
+
+
+
