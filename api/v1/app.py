709d5f41342ddcf4a4aa29533288a88b951fe49f
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-17 05:25:18 -0800

    Import CORS and set up Access-control-Allow-Origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@ register the blueprint app_views to your Flask instance app
 """
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
